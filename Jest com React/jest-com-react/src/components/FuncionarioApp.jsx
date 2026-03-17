a2d983f1a36e9da66f8da42c74934d5c5ed9f313
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-03-17 09:38:50 -0300

    Formulário Cadastro de Funcionario

--- Jest com React/jest-com-react/src/components/FuncionarioApp.jsx
+++ Jest com React/jest-com-react/src/components/FuncionarioApp.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { validarCPF } from "../utils/funcionarioUtils";
+import { Buttons } from "@testing-library/user-event/dist/cjs/system/pointer/buttons.js";
+import "./FuncionarioApp.css"
 
 export function FuncionarioApp(){
 
@@ -16,9 +18,20 @@ export function FuncionarioApp(){
 
     const [erro, setErro] = useState('')
 
+
     return (
         <div>
             <h1>Cadastro de Funcionários</h1>
+            <form style={{ display: "flex", flexDirection: "column", gap: "10px", width:"20%", margin:"auto"}} >
+                <input name="nome" placeholder="Digite o seu nome" value={form.nome}/>
+                <input name="cargo" placeholder="Digite o seu cargo" value={form.cargo}/>
+                <input name="cpf" placeholder="Digite o seu cpf" value={form.cpf}/>
+                <input name="telefone" placeholder="Digite o seu telefone" value={form.telefone}/>
+                <input name="salario" placeholder="Digite o seu salario" value={form.salario}/>
+                <input name="cep" placeholder="Digite o seu cep" value={form.cep}/>
+
+                <button type="submit">Adicionar</button>
+            </form>
         </div>
     );
 
