f9adf35959679f0acc14e5041325c2f56c4f7f82
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-03-18 09:33:35 -0300

    Update FuncionarioApp.test.jsx

--- Jest com React/jest-com-react/src/components/FuncionarioApp.test.jsx
+++ Jest com React/jest-com-react/src/components/FuncionarioApp.test.jsx
@@ -16,7 +16,7 @@ describe("Testes da Tela Cadastro de Funcionários", () => {
 
     })
 
-    test("Verificar exibição de erro ao preencher formulário", async () => {
+    test("Verificar exibição de erro ao preencher formulário completamente vazio", async () => {
         render(<FuncionarioApp />)
 
         const user = userEvent.setup()
@@ -27,6 +27,26 @@ describe("Testes da Tela Cadastro de Funcionários", () => {
 
 
     })
+
+    test("Verificar exibição de erro ao tentar cadastrar funcionário sem nome", async () => {
+        render(<FuncionarioApp />)
+
+        const user = userEvent.setup()
+
+        await user.type(screen.getByPlaceholderText("Digite o seu cargo"), "Vendedor")
+        await user.type(screen.getByPlaceholderText("Digite o seu cpf"), "12345678910")
+        await user.type(screen.getByPlaceholderText("Digite o seu salario"), "5000.00")
+        await user.type(screen.getByPlaceholderText("Digite o seu telefone"), "8598965423")
+        await user.type(screen.getByPlaceholderText("Digite o seu cep"), "12345610")
+
+        await user.click(screen.getByRole('button', { name: /adicionar/i }))
+
+        expect(screen.getByRole('alert')).toHaveTextContent('Preencha todos os campos!')
+    })
+
+    // Crie um teste que verifica o preenchimento do formulário completo porém com erro de cpf
+
+
 
 
 })
