203726d0d10ae81040fa5c6b333a50af4080d9bc
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-03-18 10:41:07 -0300

    Testes com Jest

--- Jest com React/jest-com-react/src/components/FuncionarioApp.test.jsx
+++ Jest com React/jest-com-react/src/components/FuncionarioApp.test.jsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import { FuncionarioApp } from "./FuncionarioApp"
 
+
 describe("Testes da Tela Cadastro de Funcionários", () => {
     test("Verificar renderização dos campos do formulário", () => {
         render(<FuncionarioApp />)
@@ -46,7 +47,55 @@ describe("Testes da Tela Cadastro de Funcionários", () => {
 
     // Crie um teste que verifica o preenchimento do formulário completo porém com cpf inválido
 
+    test("Verificar exibição de erro ao tentar cadastrar funcionário com cpf inválido", async () => {
+        render(<FuncionarioApp />)
+
+        const user = userEvent.setup()
+
+        await user.type(screen.getByPlaceholderText("Digite o seu nome"), "Mario")
+        await user.type(screen.getByPlaceholderText("Digite o seu cargo"), "Vendedor")
+        await user.type(screen.getByPlaceholderText("Digite o seu cpf"), "12345678")
+        await user.type(screen.getByPlaceholderText("Digite o seu salario"), "5000.00")
+        await user.type(screen.getByPlaceholderText("Digite o seu telefone"), "8598965423")
+        await user.type(screen.getByPlaceholderText("Digite o seu cep"), "12345610")
+
+        await user.click(screen.getByRole('button', { name: /adicionar/i }))
+
+        expect(screen.getByRole('alert')).toHaveTextContent('CPF INVÁLIDO!')
+    })
+
     // Crie um teste que verifica a inserção correta de um usuário
+
+    test("Verificar inserção de funcionário válido", async () => {
+        render(<FuncionarioApp />)
+
+        const user = userEvent.setup()
+
+        const f = {
+            nome: "Mario",
+            cargo: "Vendedor",
+            cpf: "52365158072",
+            salario: "5000.00",
+            telefone: "8598965423",
+            cep: "12345610"
+        }
+
+        await user.type(screen.getByPlaceholderText("Digite o seu nome"), f.nome)
+        await user.type(screen.getByPlaceholderText("Digite o seu cargo"), f.cargo)
+        await user.type(screen.getByPlaceholderText("Digite o seu cpf"), f.cpf)
+        await user.type(screen.getByPlaceholderText("Digite o seu salario"), f.salario)
+        await user.type(screen.getByPlaceholderText("Digite o seu telefone"), f.telefone)
+        await user.type(screen.getByPlaceholderText("Digite o seu cep"), f.cep)
+
+        await user.click(screen.getByRole('button', { name: /adicionar/i }))
+
+        expect(screen.getByText(`${f.nome} - ${f.cargo} - ${f.cpf} - ${f.telefone} - ${f.salario} - ${f.cep}`)).toBeInTheDocument()
+
+        expect(screen.getByPlaceholderText("Digite o seu nome")).toHaveValue("")
+        expect(screen.getByPlaceholderText("Digite o seu cargo")).toHaveValue("")
+
+
+    })
 
 
 
